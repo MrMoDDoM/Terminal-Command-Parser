a1d22188d8ce89732b306f78899a3a65c0566ad5
MrMoDDoM <danielebarattieri@gmail.com>
2014-09-19 11:10:29 +0200

    Alpha tested. WORKING

---
+++ parser.h
@@ -0,0 +1,42 @@
+//------------------------------------//
+// author: Daniele Barattieri
+// email: danielebarattieri@gmail.com
+// github: @MrMoDDoM
+// file_name: parser.h
+//------------------------------------//
+// Questo programma automatizza le procedure di acquisizione dei dati da riga di comando
+// Mette a disposizione funzioni che, ricevendo in ingresso la riga di comando utilizzata per chiamare il programma
+// Controlla la presenza di "flag" (es. -f) e restituisce il tipo di dato richiesto, successivo al flag.
+// Per esempio, chiamando "./es -f 123" e utilizzando la funzione 'flagToInt", verra' restituito il valore '123'.
+//------------------------------------//
+
+#include <stdio.h>
+#include <string.h>
+
+#if !defined TRUE
+        #define TRUE 1
+#endif
+
+#if !defined FALSE
+        #define FALSE 0
+#endif
+
+//---------HEADER----------------//
+
+//-----------Funzioni pubbliche-------------//
+int flagToInt(char *input[], char *flag);
+
+char *flagToString(char *input[], char *flag);
+
+int checkFlag(char *input[], char *flag);
+
+int posToInt(char *input[], int pos);
+
+char *posToString(char *input[], int pos);
+//------------------------------------------//
+
+//Funzioni private. NON USARE
+
+int checkInt(char *input);
+
+//----------------------------//
